8b0876568361a4307ae3d00c53543eebc84775a3
romardoss <romassuper@gmail.com>
2022-10-08 18:04:21 +0300

    Games class variables renamed

--- Lab_1/Lab_1/Games.cs
+++ Lab_1/Lab_1/Games.cs
@@ -1,7 +1,19 @@
-﻿namespace Lab_1
+﻿
+namespace Lab_1
 {
     internal class Games
     {
-        
+        public bool IsWin { get; }
+        public GameAccount Player { get; }
+        public GameAccount Opponent { get; }
+        public int Rating { get; }
+
+        public Games(bool isWin, int rating, GameAccount player, GameAccount opponent)
+        {
+            this.IsWin = isWin;
+            this.Player = player;
+            this.Opponent = opponent;
+            this.Rating = rating;
+        }
     }
 }
