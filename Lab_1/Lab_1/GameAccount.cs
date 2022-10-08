2da42c1e72f74ce1b2d3e7a09ba36c47e28e329b
romardoss <romassuper@gmail.com>
2022-10-08 15:01:42 +0300

    variables and constructor of GameAccount class are created

--- Lab_1/Lab_1/GameAccount.cs
+++ Lab_1/Lab_1/GameAccount.cs
@@ -1,7 +1,26 @@
 ﻿
+using System;
+using System.Collections.Generic;
+
 namespace Lab_1
 {
     internal class GameAccount
     {
+        public string UserName { get; }
+        public int CurrentRating { get; }
+        public int GamesCount { get; }
+        private static List<string> AllNames = new List<string>();
+
+        public GameAccount(string name)
+        {
+            if (AllNames.Contains(name))
+            {
+                throw new ArgumentException("This name is unavailable");
+            }
+            UserName = name;
+            AllNames.Add(name);
+            GamesCount = 0;
+            CurrentRating = 1;
+        }
     }
 }
