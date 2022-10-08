b53827c9b56154c6d9013fa5c757090484170040
romardoss <romassuper@gmail.com>
2022-10-08 18:27:21 +0300

    WinGame modifyed

--- Lab_1/Lab_1/GameAccount.cs
+++ Lab_1/Lab_1/GameAccount.cs
@@ -10,7 +10,7 @@ namespace Lab_1
         public int CurrentRating { get; }
         public int GamesCount { get; }
         private static List<string> AllNames = new List<string>();
-        public static List<Games> AllGames = new List<Games>();
+        public List<Games> AllGames = new List<Games>();
 
         public GameAccount(string name)
         {
@@ -26,8 +26,23 @@ namespace Lab_1
 
         public void WinGame(GameAccount opponent, int rating)
         {
+            if(rating <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rating), "Argument must be positive");
+            }
+
             var game = new Games(true, rating, this, opponent);
             AllGames.Add(game);
+            // adding this game to this account
+
+            game = new Games(false, rating, opponent, this); 
+            opponent.AllGames.Add(game);
+            // adding this game to the opponent account
+        }
+
+        public void LoseGame()
+        {
+
         }
 
     }
