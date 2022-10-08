abdf993e4807ea24747e4810a00476d78e2945ef
romardoss <romassuper@gmail.com>
2022-10-08 18:57:40 +0300

    game indexes added

--- Lab_1/Lab_1/Games.cs
+++ Lab_1/Lab_1/Games.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Lab_1
 {
     internal class Games
@@ -7,9 +9,13 @@ namespace Lab_1
         public GameAccount Player { get; }
         public GameAccount Opponent { get; }
         public int Rating { get; }
+        private static int GameID = 28462;
+        public string GameIndex { get; }
 
         public Games(bool isWin, int rating, GameAccount player, GameAccount opponent)
         {
+            this.GameIndex = GameID.ToString();
+            GameID++;
             this.IsWin = isWin;
             this.Player = player;
             this.Opponent = opponent;
